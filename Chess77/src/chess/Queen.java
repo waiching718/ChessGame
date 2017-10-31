e93fa1d8bbead9ee37d3c65477846f445bf6b31e
Wai Ching Li <wai.c.718@gmail.com>
2017-10-31 02:43:29 -0400

    updated Knight, Bishop, Queen

--- Chess77/src/chess/Queen.java
+++ Chess77/src/chess/Queen.java
@@ -7,7 +7,59 @@ public class Queen extends piece{
 	}
 	
 	public boolean isvalidmove(int file, int rank){
-		return false;
+		if(this.file == file && this.rank == rank){ // remain the same place
+			return false;
+		}
+		// Below, Queen share the similar function with bishop and rook.
+		int x,y;
+		
+		if(this.file > file){ // x to be stored to 1 or -1 for checking every column between 
+			x = -1;		  // the piece's location and destination.
+		}else{
+			x= 1;
+		}
+		
+		if(this.rank > rank){ //y to be stored to 1 or -1 for checking every row between
+			y = -1;		  // the piece's location and destination.
+		}else{
+			y = 1;
+		}
+		int row; 
+		int col; 
+		col = file + x; // The first position to check
+		
+		if(this.black){ // For black bishop,checking diagonally 
+			for(row = rank + y; row < this.rank; row = row + y ){ // Check if there is a blocking piece.
+				if(board[row][col] != null && board[row][col].black == true){ // Can not move there if that is a black piece
+				return false;
+				}
+				col = col + x;
+			}
+		}else{ // For White bishop,checking diagonally
+			for(row = rank + y; row < this.rank; row = row + y ){ // Check if there is a blocking piece.
+				if(board[row][col] != null && board[row][col].black == false){ // Can not move there if that is a white peice
+				return false;
+				}
+				col = col + x;
+			}
+		}
+		/*if(this.black){ // For black bishop, checking horizontally and vertically 
+			for(row = rank + y; row < this.rank; row = row + y ){ // Check if there is a blocking piece.
+				if(board[row][col] != null && board[row][col].black == true){ // Can not move there if that is a black piece
+				return false;
+				}
+				col = col + x;
+			}
+		}else{ // For White bishop, checking horizontally and vertically
+			for(row = rank + y; row < this.rank; row = row + y ){ // Check if there is a blocking piece.
+				if(board[row][col] != null && board[row][col].black == false){ // Can not move there if that is a white peice
+				return false;
+				}
+				col = col + x;
+			}
+		}*/
+		
+		return true;
 	}
 	
 }
